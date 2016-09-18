5d9aed7456f6626fa33894148beff9f91c7a7893
tarun raja <tarun17170@gmail.com>
2016-09-18 11:54:08 -0500

    changes

--- src/js/controller/dep.js
+++ src/js/controller/dep.js
@@ -1,24 +1,28 @@
-angular.module('app',['ngMaterial','ngAnimate','ngRoute','ngMessages','app.chat.header', 'app.login', 'app.register'])
-	.config(['$routeProvider','$locationProvider',
+angular.module('app',['ngMaterial','ngAnimate','ngRoute','ngMessages','app.chat.header', 'app.login', 'app.register', 'app.user.main.page','app.user.contacts'])
+    .config(['$routeProvider','$locationProvider',
         function($routeProvider, $locationProvider) {
             $routeProvider
                 .when('/login', {
-                	path: "/login",
-                    templateUrl:"templates/pages/login.html",
+                    path: "/login",
+                    templateUrl:"templates/login.html",
                     controller:'logController'
                 })
+                .when('/users/:name', {
+                    templateUrl:"templates/userPage.html",
+                    controller:'UserController'
+                })
                 .when('/register', {
-                	path: "/register",
-                    templateUrl:"templates/pages/login.html",
+                    templateUrl:"templates/login.html",
                     controller:'regController'
-                  })
-               .when('/user/contacts',{
-                   templateUrl:"templates/pages/contacts.html",
-                     controller:'contactsController'
-                  });
+                })
+                .when('/user/contacts',{
+                    templateUrl:"templates/pages/contacts.html",
+                    controller:'contactsController'
+                });
+                
             }])
-	 .controller('mainCntrl',['$routeParams',function($routeParams)
+     .controller('mainCntrl',['$routeParams',function($routeParams)
     {
       
         this.params = $routeParams;
-    }])
+    }]);
