753804effd85b4b1f131d2886369bbc94f552c54
tarun17170@gmail.com <tarun17170@gmail.com>
2016-10-10 22:51:11 -0500

    chat

--- config/strategies/google.strategy.js
+++ config/strategies/google.strategy.js
@@ -5,7 +5,7 @@ module.exports = function(){
 	passport.use(new GoogleStrategy({
 		clientID: '608708050096-d6dokb370jkaguc5vgcaigmp060grkrt.apps.googleusercontent.com',
 		clientSecret: 'G3mTns-ncojrZkh0hQOnbXIk',
-		callbackURL: 'http://localhost:3000/auth/google/callback'},
+		callbackURL: 'http://tarunblog.com/auth/google/callback'},
 		function(req, accessToken, refreshToken, profile, done){
 			done(null,profile);
 		})
